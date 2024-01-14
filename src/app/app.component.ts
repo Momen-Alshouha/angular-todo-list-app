feefa9d21466604de3e63a6bf191b441b8893282
momen_alshouha <momenalshouha@outlook.com>
2024-01-14 12:52:18 +0300

    list-manager component generated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,20 @@
 import { Component } from '@angular/core';
-import { TodoItem } from './interfaces/todo-item';
-import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
-import { TodoItemComponent } from './todo-item/todo-item.component';
-import { NgFor } from '@angular/common';
 import { CommonModule } from '@angular/common';
+import { ListManagerComponent } from './list-manager/list-manager.component';
 
 @Component({
   selector: 'app-root',
-  standalone:true,
-  imports:[TodoItemComponent,InputButtonUnitComponent,NgFor,CommonModule],
+  standalone: true,
+  imports: [CommonModule,ListManagerComponent],
   template: `
-    <h1>
-      Welcome to {{ title }}!
-    </h1>
+  <h1>
+    Welcome to {{ title }}!
+  </h1>
 
-    <app-input-button-unit (submit)="addItem($event)"></app-input-button-unit>
-
-    <ul>
-    <li *ngFor="let todoItem of todoList; let i = index; trackBy: trackByFn">
-    <app-todo-item [item]="todoItem"></app-todo-item>
-    </li>
-
-    </ul>
-  `,
-  styleUrls: ['./app.component.scss']
+  <app-list-manager></app-list-manager>
+`,
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'todo-list';
-  todoList: TodoItem[] = [
-    {title: 'install NodeJS'},
-    {title: 'create new app'},
-  ];
-
-  addItem(title: string): void {
-    console.log(title);
-    this.todoList.push({ title });
-  }
-
-  trackByFn(index: number, item: TodoItem): number {
-    return index;
-  }
-  
-
+  title = 'My To-Do List App';
 }
---
+++ src/app/list-manager/list-manager.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { TodoItem } from '../interfaces/todo-item';
+import { InputButtonUnitComponent } from '../input-button-unit/input-button-unit.component';
+import { TodoItemComponent } from '../todo-item/todo-item.component';
+import { NgFor } from '@angular/common';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-list-manager',
+  standalone:true,
+  imports:[TodoItemComponent,InputButtonUnitComponent,NgFor,CommonModule],
+  template: `
+    <h1>
+      Welcome to {{ title }}!
+    </h1>
+
+    <app-input-button-unit (submit)="addItem($event)"></app-input-button-unit>
+
+    <ul>
+    <li *ngFor="let todoItem of todoList; let i = index; trackBy: trackByFn">
+    <app-todo-item [item]="todoItem"></app-todo-item>
+    </li>
+
+    </ul>
+  `,
+  styleUrls: ['./list-manager.component.scss']
+})
+export class ListManagerComponent {
+  title = 'todo-list';
+  todoList: TodoItem[] = [
+    {title: 'install NodeJS'},
+    {title: 'create new app'},
+  ];
+
+  addItem(title: string): void {
+    console.log(title);
+    this.todoList.push({ title });
+  }
+
+  trackByFn(index: number, item: TodoItem): number {
+    return index;
+  }
+  
+
+}
