2d5cf528be42a2561788485a68cef37f7f752cd6
momen_alshouha <momenalshouha@outlook.com>
2024-01-14 11:15:49 +0300

    present to-do list items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [CommonModule, RouterOutlet,InputButtonUnitComponent],
+  template: `
+  <h1>
+  Welcome to {{ title }}!
+</h1>
+
+<app-input-button-unit></app-input-button-unit>
+
+<ul>
+  <li *ngFor="let todoItem of todoList; track: title">
+    {{ todoItem.title }}
+  </li>
+</ul>
+
+`, 
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-todo-list-app';
+  title = 'todo-list';
+  todoList = [
+    {title: 'install NodeJS'},
+    {title: 'install Angular CLI'},
+    {title: 'create new app'},
+    {title: 'serve app'},
+    {title: 'develop app'},
+    {title: 'deploy app'},
+  ];
 }
--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-input-button-unit',
   standalone: true,
-  imports: [],
-  templateUrl: './input-button-unit.component.html',
+  imports: [CommonModule],
+  template: `
+  <input #inputElementRef
+         [value]="title"
+         (keyup.enter)="changeTitle(getInputValue($event))">
+
+  <button (click)="changeTitle(inputElementRef.value)">
+    Save
+  </button>
+`,
   styleUrl: './input-button-unit.component.scss'
 })
 export class InputButtonUnitComponent {
-  title = 'Learn about components';
+  title = 'Hello World';
+
+  changeTitle(newTitle: string) {
+    this.title = newTitle;
+  }
+  
+  getInputValue(event: Event) {
+    return (event.target as HTMLInputElement).value;
+  }
 }
