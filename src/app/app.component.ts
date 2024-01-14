db624aa15b21610f878907a043bf973df835f10a
momen-alshouha <momenalshouha@outlook.com>
2024-01-14 21:56:00 +0300

    footer created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListManagerComponent } from './list-manager/list-manager.component';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import { FooterComponent } from './footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,ListManagerComponent,NavbarComponent],
+  imports: [CommonModule,ListManagerComponent,NavbarComponent,FooterComponent],
   template: `
   <app-navbar></app-navbar>
   <h1 class="app-title">
@@ -14,6 +14,7 @@ import { NavbarComponent } from './navbar/navbar.component';
   </h1>
 
   <app-list-manager></app-list-manager>
+  <app-footer></app-footer>
 `,
   styleUrl: './app.component.scss'
 })
