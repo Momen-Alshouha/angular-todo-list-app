cf39da64d56bb32710aea2229d03fbc01926536b
momen-alshouha <momenalshouha@outlook.com>
2024-01-14 21:47:56 +0300

    navbar component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListManagerComponent } from './list-manager/list-manager.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,ListManagerComponent],
+  imports: [CommonModule,ListManagerComponent,NavbarComponent],
   template: `
+  <app-navbar></app-navbar>
   <h1 class="app-title">
     Welcome to {{ title }}!
   </h1>
